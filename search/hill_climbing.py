76345bf45269d272c45845b9d1d9446ff4d719b2
atieh <atgorgin@gmail.com>
2026-05-30 22:01:14 +0330

    add hill climbing algorithm

--- search/hill_climbing.py
+++ search/hill_climbing.py
@@ -1,28 +1,38 @@
 from search.local_search_base import LocalSearchBase
 
-
 class HillClimbing(LocalSearchBase):
-    def run(self, initial_state, **kwargs):
-        """
-        TODO: Implement the Hill Climbing algorithm.
-        
-        Parameters
-        ----------
-        initial_state : list of tuples
-            The initial configuration of sensors.
-        **kwargs : 
-            Define and add any other parameters you might need for the algorithm 
+    def run(self, initial_state,iterations=500,neighbors=40):
+        current_state = initial_state.copy()
+        current_cost = self.evaluate(current_state)
 
-        Returns
-        -------
-        best_state : list of tuples
-            The best configuration found.
-        best_cost : int or float
-            The cost of the best configuration.
-        evaluations : list
-            List of costs at each iteration (used for plotting).
-        states_history : list of lists
-            List of states at each iteration (used for animation).
-        """
-        
-        raise NotImplementedError("Students must implement this method.")
+        best_state = current_state
+        best_cost = current_cost
+
+        evaluations = [current_cost]
+        states_history = [current_state]
+
+        for i in range(iterations):
+            best_neighbor = None
+            best_neighbor_cost = float("inf")
+
+            for j in range(neighbors):
+                neighbor = self.get_neighbor(current_state)
+                cost = self.evaluate(neighbor)
+                if cost < best_neighbor_cost:
+                    best_neighbor_cost = cost
+                    best_neighbor = neighbor
+
+            if best_neighbor_cost >= current_cost:
+                break
+
+            current_state = best_neighbor
+            current_cost = best_neighbor_cost
+
+            if current_cost < best_cost:
+                best_cost = current_cost
+                best_state = current_state.copy()
+
+            evaluations.append(current_cost)
+            states_history.append(current_state.copy())
+
+        return (best_state, best_cost, evaluations, states_history)
